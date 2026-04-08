8cf7b5f234e9d53bc364b32c075c61fc491cb438
RM24004 <167376455+RM24004@users.noreply.github.com>
2026-04-07 21:17:21 -0600

    feat: add base backend project
    Primer commit del backend con todas las dependencias iniciales y
    application.properties configurado para PostgreSQL.

---
+++ backend/src/main/java/com/group1/proyect/freshbasket/FreshbasketApplication.java
@@ -0,0 +1,13 @@
+package com.group1.proyect.freshbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshbasketApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/group1/proyect/freshbasket/FreshbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.proyect.freshbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
